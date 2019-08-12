f1b09a63855c1a52e4dcaafc14e5d9b3262d8f65
austinStotts <austintstotts@gmail.com>
2019-08-12 16:18:56 -0500

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'rgbx.org',
+    script: './server/index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-198-213-221.compute-1.amazonaws.com',
+      key: '~/.ssh/austin.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:austinStotts/project.rgbx.git',
+      path: '/home/ubuntu/rgbx',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
